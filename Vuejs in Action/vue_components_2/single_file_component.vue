f7a878463b4779b36df284c9dce942cebfb0d668
Grigory <gakhromov@MiAir13.localdomain>
2021-01-18 22:24:30 +0300

    chapter 6 done

---
+++ Vuejs in Action/vue_components_2/single_file_component.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "hello",
+    data: {
+        msg: "Welcome to your Vue.js app!"
+    }
+}
+</script>
+
+<style scoped>
+.hello {
+    color: brown;
+}
+</style>
