ddd7edcfc7c568efe9ca8bc86682f7717665ccba
Grigory <gakhromov@MiAir13.localdomain>
2021-01-20 00:00:52 +0300

    chapter 7 part 6 in progress

---
+++ Vuejs in Action/second_vue_app/petstore/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <div class="navbar navbar-light bg-light">
+            <a href="#" class="navbar-brand mb0 h1">
+                <router-link v-bind:to="{name: 'iMain'}">
+                    {{sitename}}
+                </router-link>
+            </a>
+            <button class="btn btn-secondary" v-on:click="showCheckout">
+                {{ cartItemCount }}
+                <span class="fas fa-shopping-cart"></span>
+                Checkout
+            </button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'my-header',
+    data: {
+        sitename: "Vue.js Pet Depot"
+    },
+    props: ["cartItemCount"],
+    methods: {
+        showCheckout() {
+            this.$router.push({name: 'Form'});
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+    color: black;
+}
+</style>
---
+++ Vuejs in Action/second_vue_app/petstore/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Form from '@/components/Form'
+import Main from '@/components/Main'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'iMain',
+    component: Main,
+    props: true,
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    component: Form,
+    props: true,
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
